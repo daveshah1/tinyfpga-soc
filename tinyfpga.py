ee22df2233da7676941e1734ef12196f5c968d62
William D. Jones <thor0505@comcast.net>
2017-11-11 10:41:36 -0500

    Initial commit. Successful compile, not tested.

---
+++ tinyfpga.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import argparse
+import importlib
+import os.path
+import struct
+
+from migen import *
+# from migen.genlib.io import CRG
+from migen.build.platforms import tinyfpga_b
+from migen.build.generic_platform import *
+
+from misoc.integration.soc_core import *
+from misoc.integration.builder import *
+
+
+class BaseSoC(SoCCore):
+    def __init__(self, platform, **kwargs):
+        SoCCore.__init__(self, platform,
+            clk_freq=int((1/(platform.default_clk_period))*1000000000),
+            integrated_rom_size=0x200,
+            integrated_sram_size=0x1000,
+            integrated_main_ram_size=0,
+            **kwargs)
+        self.submodules.crg = CRG(platform.request(platform.default_clk_name))
+
+
+# BIOS + RAM + Block RAM used for design is too much. Customize instead.
+# Also, `builder = Builder(soc, **builder_argdict(args))
+# builder._initialize_rom = _initialize_rom_tinyfpga`
+# Doesn't work... why?
+class TinyFpgaBuilder(Builder):
+    def __init__(self, soc, output_dir=None,
+                 compile_software=True, compile_gateware=True,
+                 gateware_toolchain_path=None,
+                 csr_csv=None):
+        Builder.__init__(self, soc, output_dir, compile_software,
+            compile_gateware, gateware_toolchain_path, csr_csv)
+        self.software_packages = []
+        self.add_software_package("loader",
+            os.path.abspath(os.path.join(os.path.dirname(__file__), "loader")))
+
+    def _initialize_rom(self):
+        bios_file = os.path.join(self.output_dir, "software", "loader",
+                                 "loader.bin")
+        if self.soc.integrated_rom_size:
+            with open(bios_file, "rb") as boot_file:
+                boot_data = []
+                while True:
+                    w = boot_file.read(4)
+                    if not w:
+                        break
+                    boot_data.append(struct.unpack(">I", w)[0])
+            self.soc.initialize_rom(boot_data)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="TinyFPGA MiSoC port")
+    builder_args(parser)
+    soc_core_args(parser)
+    # parser.add_argument("--with-ethernet", action="store_true",
+    #                    help="enable Ethernet support")
+    args = parser.parse_args()
+
+    platform = tinyfpga_b.Platform()
+    serial = [("serial", 0,
+                    Subsignal("tx", Pins("GPIO:0")),
+                    Subsignal("rx", Pins("GPIO:1")),
+             )]
+    platform.add_extension(serial)
+    # cls = MiniSoC if args.with_ethernet else BaseSoC
+    # soc = cls(platform, integrated_rom_size=0xa000, **soc_core_argdict(args))
+    soc = BaseSoC(platform, **soc_core_argdict(args))
+
+    # We want a custom lm32_config.v. Since there's no clean way to really
+    # remove include paths, let's go ahead and manually modify it.
+    lm32_config_paths = list(platform.verilog_include_paths)
+    platform.verilog_include_paths.remove(lm32_config_paths[0])
+    platform.add_verilog_include_path(".")
+    # Hack to workaround potential bug in yosys where include files aren't
+    # picked up properly?
+    platform.add_verilog_include_path(os.path.join(lm32_config_paths[0], "submodule", "rtl"))
+
+    builder = TinyFpgaBuilder(soc, **builder_argdict(args))
+    builder.build()
+
+
+if __name__ == "__main__":
+    main()
