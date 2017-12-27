a0aebc4fc4cc597fe534e6f8561b8a79506bf607
William D. Jones <thor0505@comcast.net>
2017-12-27 16:31:43 -0500

    Place the BIOS into SPI Flash, remove loader. Design compiles.

--- tinyfpga.py
+++ tinyfpga.py
@@ -12,46 +12,25 @@ from litex.build.generic_platform import *
 
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
+from litex.soc.cores.spi_flash import SpiFlashSingle
 
 
 class BaseSoC(SoCCore):
     def __init__(self, platform, **kwargs):
+        # TinyFPGA bootloader uses warmboot- user bitstrem by default starts at 0x30000.
+        # 8k bitstream is exactly 136448 bytes. We lose a decent amount of space to
+        # this.
         SoCCore.__init__(self, platform,
             clk_freq=int((1/(platform.default_clk_period))*1000000000),
-            integrated_rom_size=0x400,
+            cpu_reset_address=0x30000 + 136448,
+            integrated_rom_size=0,
             integrated_sram_size=0x2800,
             integrated_main_ram_size=0,
             **kwargs)
+
         self.submodules.crg = CRG(platform.request(platform.default_clk_name))
-
-
-# BIOS + RAM + Block RAM used for design is too much. Customize instead.
-# Also, `builder = Builder(soc, **builder_argdict(args))
-# builder._initialize_rom = _initialize_rom_tinyfpga`
-# Doesn't work... why?
-class TinyFpgaBuilder(Builder):
-    def __init__(self, soc, output_dir=None,
-                 compile_software=True, compile_gateware=True,
-                 gateware_toolchain_path=None,
-                 csr_csv=None):
-        Builder.__init__(self, soc, output_dir, compile_software,
-            compile_gateware, gateware_toolchain_path, csr_csv)
-        self.software_packages = []
-        self.add_software_package("loader",
-            os.path.abspath(os.path.join(os.path.dirname(__file__), "loader")))
-
-    def _initialize_rom(self):
-        bios_file = os.path.join(self.output_dir, "software", "loader",
-                                 "loader.bin")
-        if self.soc.integrated_rom_size:
-            with open(bios_file, "rb") as boot_file:
-                boot_data = []
-                while True:
-                    w = boot_file.read(4)
-                    if not w:
-                        break
-                    boot_data.append(struct.unpack(">I", w)[0])
-            self.soc.initialize_rom(boot_data)
+        self.submodules.spiflash = SpiFlashSingle(platform.request("spiflash"))
+        self.register_rom(self.spiflash.bus)
 
 
 def main():
@@ -70,7 +49,7 @@ def main():
     platform.verilog_include_paths.remove(lm32_config_paths[0])
     platform.add_verilog_include_path(".")
 
-    builder = TinyFpgaBuilder(soc, **builder_argdict(args))
+    builder = Builder(soc, **builder_argdict(args))
     builder.build()
 
 
